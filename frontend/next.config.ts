d1af86524f226b0871318a420a14fe2d03999d1b
otto <ottobjonas@outlook.com>
2025-06-02 10:38:34 +0100

    fixed couple things

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,46 +1,51 @@
 /** @type {import('next').NextConfig} */
 interface WebpackRule {
-    test: RegExp; 
-    exclude?: RegExp; 
-    use: {
-        loader: string; 
+  test: RegExp;
+  exclude?: RegExp;
+  use:
+    | {
+        loader: string;
         options: {
-            presets: string[]; 
-            plugins: string[]; 
-        }
-    } | {
-        loader: string; 
+          presets: string[];
+          plugins: string[];
+        };
+      }
+    | {
+        loader: string;
         options: {
-            name: string; 
-            outputPath: string; 
-            publicPath: string; 
-        }
-    }[]; 
+          name: string;
+          outputPath: string;
+          publicPath: string;
+        };
+      }[];
 }
 
 interface NextConfig {
-    webpack: (config:any) => any; 
-    distDir: string; 
-    reactStrictMode: boolean; 
-    rewrites: () => Promise<{ source: string; destination: string }[]>; 
-    assetPrefix: string; 
+  webpack: (config: any) => any;
+  distDir: string;
+  reactStrictMode: boolean;
+  rewrites: () => Promise<{ source: string; destination: string }[]>;
+  assetPrefix: string;
 }
 
-const nextConfig = NextConfig = {
-    webpack: (config) => {
-        config.module.rules.push(
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader", 
-                    options: {
-                        presets: {
-                            
-                        }
-                    }
-                }
-            }
-        )
-    }
-}
+const config: NextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: {},
+        },
+      },
+    });
+    return config; // Make sure to return the config!
+  },
+  distDir: "dist",
+  reactStrictMode: true,
+  rewrites: async () => [{ source: "/old-path", destination: "/new-path" }],
+  assetPrefix: "",
+};
+
+export default config;
