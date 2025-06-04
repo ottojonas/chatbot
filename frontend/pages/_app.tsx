21fd7ea25fb195150ac1fb39ac28ae97836f9691
otto <ottobjonas@outlook.com>
2025-06-04 11:06:12 +0100

    analytics

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "../styles/globals.css";
 import { AuthProvider } from "../context/AuthContext";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+      <Analytics />
     </AuthProvider>
   );
 }
